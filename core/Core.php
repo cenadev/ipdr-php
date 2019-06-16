c1db24e818582c8d39df49daa4110639666ef4ae
cleisonsantos <css.cleison@outlook.com>
2019-06-16 01:19:23 -0300

    Autoload configurado e .htaccess de backup salvo.

--- core/Core.php
+++ core/Core.php
@@ -5,13 +5,23 @@ class Core {
     private $currentAction;
     
     public function run(){
-        $url = substr($_SERVER['PHP_SELF'], 10);
+        $url = substr($_SERVER['PHP_SELF'], 19);
 
         if(!empty($url)){
-            echo $url;
-        }else{
+            $url = explode('/', $url);
+            array_shift($url);
+
+            $this->currentController = $url[0].'Controller';
+            if(isset($url[1])){
+                $this->currentAction = $url[1];
+            } else{
+                $this->currentAction = 'index';
+            }
+
+        } else {
             $this->currentController = 'homeController';
             $this->currentAction = 'index';
         }
+        
     }
 }
