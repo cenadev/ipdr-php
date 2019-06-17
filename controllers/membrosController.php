9fd3342145eba8e7ee2b2abb98f283699bdc008f
cleisonsantos <css.cleison@outlook.com>
2019-06-16 21:32:41 -0300

    Criado o controller membros.

---
+++ controllers/membrosController.php
@@ -0,0 +1,11 @@
+<?php
+class membrosController extends Controller {
+    public function index()
+    {
+        echo 'Esta é a lista de membros';
+    }
+
+    public function cadastrar(){
+        echo 'Essa é a página de cadastro';
+    }
+}
