f51f123e15e28065c171d7ccb3ddd25b6935c9e4
cleisonsantos <css.cleison@outlook.com>
2019-06-16 21:29:57 -0300

    Rota com a url ataptavel.

--- core/Core.php
+++ core/Core.php
@@ -3,8 +3,10 @@
 class Core {
     
     public function run(){
-        $url = substr($_SERVER['PHP_SELF'], 19);
+        $url = explode('index.php', $_SERVER['PHP_SELF']);
 
+        $url = end($url);
+        
         if(!empty($url)){
             $url = explode('/', $url);
             array_shift($url);
