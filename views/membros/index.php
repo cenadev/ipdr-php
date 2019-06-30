5fecfe5ca5fc1c508ca3329011ceabf6cf28d587
cleisonsantos <css.cleison@outlook.com>
2019-06-29 21:33:54 -0300

    Adicionado os botões de ação nos membros.

--- views/membros/index.php
+++ views/membros/index.php
@@ -1,1 +1,3 @@
-<h1>Está é a index</h1>
+<a href="<?php echo (BASE_URL); ?>/membros/cadastrar" class="btn btn-outline-primary">Cadastrar</a>
+<a href="<?php echo (BASE_URL); ?>/membros/alterar" class="btn btn-outline-info">Alterar</a>
+<a href="<?php echo (BASE_URL); ?>/membros/alterar" class="btn btn-outline-danger">Excluir</a>
