800bac31508bddf93ff4b68838045a59b730952e
cleisonsantos <css.cleison@outlook.com>
2019-07-04 07:41:42 -0300

    Adicionado o jqury e ajax para o cadastro

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,26 @@
-$('#cad1Form').submit(function (e) {
-    e.preventDefault();
-    //e.children[0].attr("style", "display:inline-block");
+var msgAlert = '';
+$('#spinnerModal1').hide();
+$('#cadModal1Submit').click(function (){
+    $(this).html("Salvando"), $(this).attr('disabled', ''),$('#spinnerModal1').show();
 
-})
+    $.ajax({
+        type: "post",
+        url: "/membros/cadastrar",
+        data: {
+            'nome': $("#inputName").val(),
+            'genero': $("#genderRadios").val(),
+            'dataNasc': $("#inputDateOfBirth").val(),
+            'nacionalidade': $("#inputNacionality").val(),
+            'natalidade': $("#inputNatality").val(),
+            'email': $("#inputEmail").val(),
+            'identidade': $("#inputRG").val(),
+            'orgaoEmissor': $("#inputShippingAgent").val(),
+            'inputCPF': $("#inputCPF").val()
+        },
+        dataType: "html",
+        success: function (response) {
+            msgAlert = 'Cadastro realizado com sucesso!'
+            $('#alertModal1').html(msgAlert), $('#alertModal1').addClass('alert-sucess'), $('#alertModal1').alert();
+        }
+    });
+});
