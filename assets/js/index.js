129edf8064a4d7546233eb88964639c7c6f67d7a
cleisonsantos <css.cleison@outlook.com>
2019-07-08 15:23:37 -0300

    Envio de formulário ajax funcional com response.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,38 @@
-$(document).ready(function(){
-var msgAlert = '';
+
+let msgAlert = '';
+$("#alertModal1").hide();
 $('#spinnerModal1').hide();
-$('#cadModal1Submit').click(function (){
-    $(this).html("Salvando"), $(this).attr('disabled', ''),$('#spinnerModal1').show();
+$('#')
+$('#cad1Form').submit(function (e) {
+    e.preventDefault();
+    $("#cadModal1Submit").html("Salvando"), $("#cadModal1Submit").attr('disabled', ''), $('#spinnerModal1').show();
 
     $.ajax({
+        url: "/ipdr-php/membros/cadastrar",
         type: "post",
-        url: "/membros/cadastrar",
+        dataType: "html",
         data: {
             'nome': $("#inputName").val(),
-            'genero': $("#genderRadios").val(),
+            'genero': $("[name='genderRadios']").val(),
             'dataNasc': $("#inputDateOfBirth").val(),
-            'nacionalidade': $("#inputNacionality").val(),
+            'nacionalidade': $("#inputNationality").val(),
             'natalidade': $("#inputNatality").val(),
             'email': $("#inputEmail").val(),
             'identidade': $("#inputRG").val(),
             'orgaoEmissor': $("#inputShippingAgent").val(),
             'inputCPF': $("#inputCPF").val()
         },
-        dataType: "html",
+
         success: function (response) {
+
+            console.log("Foi");
             msgAlert = 'Cadastro realizado com sucesso!';
-            $('#alertModal1').html(msgAlert, response), $('#alertModal1').addClass('alert-sucess'), $('#alertModal1').alert();
+            console.log(response);
+            $('#alertModal1').html(msgAlert), $('#alertModal1').addClass('alert-success'), $('#alertModal1').show();
+        },
+        error: function (err) {
+            console.log("Request failed: " + err);
+
         }
     });
-});
 });
--- views/membros/index.php
+++ views/membros/index.php
@@ -100,7 +100,7 @@
                     </button>
                 </div>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary" id="cadModal1Submit" data-toggle="modal" data-dismiss="" data-target="">
+                <button type="submit" class="btn btn-primary" id="cadModal1Submit" data-toggle="modal" data-dismiss="" data-target="">
 
                     Salvar
                 </button>
