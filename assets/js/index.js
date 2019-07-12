d9b0d703a758ce3c4b13a674c46105123c6a159c
cleisonsantos <css.cleison@outlook.com>
2019-07-11 22:22:49 -0300

    Retidata as funções de alert no cadastro.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 
 let msgAlert = '';
-$("#alertModal1").hide();
+//$("#alertModal1").hide();
 $('#spinnerModal1').hide();
 //$('#')
 $('#cad1Form').submit(function (e) {
@@ -26,7 +26,7 @@ $('#cad1Form').submit(function (e) {
         success: function (response) {
             msgAlert = 'Cadastro realizado com sucesso!';
             console.log(response);
-            $('#alertModal1').html(msgAlert), $('#alertModal1').addClass('alert-success'), $('#alertModal1').show();
+            //$('#alertModal1').html(msgAlert), $('#alertModal1').addClass('alert-success'), $('#alertModal1').show();
             $("#cadModal1Submit").html("Próximo"), $("#cadModal1Submit").attr('data-dismiss', 'modal'), $("#cadModal1Submit").attr('type', 'button'), $("#cadModal1Submit").removeAttr('disabled', ''), $('#spinnerModal1').hide();
         },
         error: function (err) {
