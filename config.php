9e53bed8491fc1fc49af78ac1f1c8c635ab56775
cleisonsantos <css.cleison@outlook.com>
2019-07-21 20:06:07 -0300

    Definido a configuração do banco de dados.

--- config.php
+++ config.php
@@ -7,10 +7,10 @@ global $config;
 $config = array();
 
 if (ENVIRONMENT == "dev") {
-    $config['dbname'] = 'ipdr';
-    $config['host'] = 'localhost';
-    $config['dbuser'] = 'root';
-    $config['dbpass'] = 'root';
+    $config['dbname'] = 'cenade29_ipdr';
+    $config['host'] = '162.241.2.188';
+    $config['dbuser'] = 'cenade29_cleison';
+    $config['dbpass'] = 'senha.css';
 } else {
     $config['dbname'] = 'ipdr';
     $config['host'] = 'localhost';
