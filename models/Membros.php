e67ea932bdcdd679556d78c477a4de015c9bdc48
cleisonsantos <css.cleison@outlook.com>
2019-07-22 20:49:46 -0300

    Construido o metodo que pega os membros do banco.

--- models/Membros.php
+++ models/Membros.php
@@ -2,6 +2,13 @@
 class Membros extends Model {
 
     public function getMembers(){
-        
+        $membros = array();
+
+        $sql = "SELECT * FROM membros ";
+        $sql = $this->db->query($sql);
+        if($sql->rowCount() > 0){
+            $membros = $sql->fetchAll();
+        }
+        return $membros;
     }
 }
