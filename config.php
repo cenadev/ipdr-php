b4811a27d2f5e1503af0e24628fdaa74b6bf3974
cleisonsantos <css.cleison@outlook.com>
2019-08-06 00:59:42 -0300

    Setado a configuração de banco local.

--- config.php
+++ config.php
@@ -7,13 +7,14 @@ global $config;
 $config = array();
 
 if (ENVIRONMENT == "dev") {
+    $config['dbname'] = 'db_ipdr';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = 'root';
+} else {
+    
     $config['dbname'] = 'cenade29_ipdr';
     $config['host'] = '162.241.2.188';
     $config['dbuser'] = 'cenade29_cleison';
     $config['dbpass'] = 'senha.css';
-} else {
-    $config['dbname'] = 'ipdr';
-    $config['host'] = 'localhost';
-    $config['dbuser'] = 'root';
-    $config['dbpass'] = 'root';
 }
